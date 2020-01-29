047188b79351ca256009af816c233078ee5d2066
Yidan Ma <madt@bc.edu>
2020-01-29 10:15:13 -0500

    Initial Commit

---
+++ Word Garden/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Garden
+//
+//  Created by mayidan on 1/29/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
